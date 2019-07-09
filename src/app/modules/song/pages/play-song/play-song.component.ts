311246fa9791155c8aa5f529e262a57983bd1f0f
Wendelin Muth <wendelin.muth@signon-group.com>
2019-07-09 16:15:02 +0200

    Fixed Playlist Dialog

--- src/app/modules/song/pages/play-song/play-song.component.ts
+++ src/app/modules/song/pages/play-song/play-song.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Song, Playlist } from 'src/app/shared';
+import { Song } from 'src/app/shared';
 import { SongsService } from 'src/app/core/services';
-import { MatDialog } from '@angular/material/dialog';
-import { PlaylistSelectorDialog } from 'src/app/shared/components';
-import { PlaylistService } from 'src/app/core/services/playlist.service';
-import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-play-song',
@@ -15,22 +11,10 @@ import { Observable } from 'rxjs';
 export class PlaySongComponent implements OnInit {
 	song: Song;
 
-	constructor(
-		private route: ActivatedRoute,
-		private songServ: SongsService,
-		private dialog: MatDialog
-	) {}
+	constructor(private route: ActivatedRoute, private songServ: SongsService) {}
 
 	ngOnInit() {
 		const id = +this.route.snapshot.paramMap.get('id');
 		this.songServ.getSong(id).subscribe(song => (this.song = song));
-	}
-
-	showPlaylistDialog() {
-		const dialogRef = this.dialog.open(PlaylistSelectorDialog, { data: this.song });
-		dialogRef.afterClosed().subscribe(song => {
-			if (!song) return;
-			this.song = song;
-		});
 	}
 }
--- src/app/shared/components/song-player/song-player.component.ts
+++ src/app/shared/components/song-player/song-player.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Song } from 'src/app/shared';
+import { MatDialog } from '@angular/material/dialog';
+import { PlaylistSelectorDialog } from '..';
 
 export enum PlayerState {
 	PAUSED,
@@ -18,6 +20,8 @@ export class SongPlayerComponent implements OnInit {
 	@Input() song: Song;
 	@Output('stateChanged') stateChangedEmitter: EventEmitter<PlayerState> = new EventEmitter();
 
+	constructor(private dialog: MatDialog) {}
+
 	ngOnInit() {
 		this.emitStateChange(PlayerState.LOADING + '');
 	}
@@ -25,5 +29,13 @@ export class SongPlayerComponent implements OnInit {
 	emitStateChange(state: string) {
 		const newState = PlayerState[state];
 		this.stateChangedEmitter.emit(newState);
+	}
+
+	showPlaylistDialog() {
+		const dialogRef = this.dialog.open(PlaylistSelectorDialog, { data: this.song });
+		dialogRef.afterClosed().subscribe(song => {
+			if (!song) return;
+			this.song = song;
+		});
 	}
 }
